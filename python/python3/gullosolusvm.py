ae9c7d422fe715a99cf4a5280f0e33bf7d08cb6c
wf-nb <82250551+wf-nb@users.noreply.github.com>
2021-04-19 14:13:24 +0800

    Update gullosolusvm.py

--- python/python3/gullosolusvm.py
+++ python/python3/gullosolusvm.py
@@ -4,17 +4,17 @@ import requests
 
 class solusVM_Client():
     def init(self):
-        self.url = "solusvm.gullo.me"
-        self.key = "NQ9RU-H4ET0-AL9NO"
-        self.hash = "f1666148fc00fba83bf327eaf4af58f04eaea6e9"
-        self.bot = "1653363243:AAHx5w1nkyMto_f5G5pjPMNOoajeJdr1atI"
-        self.chat = "1054232996"
+        self.apiurl = "solusvm.gullo.me"
+        self.apikey = "NQ9RU-H4ET0-AL9NO"
+        self.apihash = "f1666148fc00fba83bf327eaf4af58f04eaea6e9"
+        self.tgbot = "1653363243:AAHx5w1nkyMto_f5G5pjPMNOoajeJdr1atI"
+        self.tgchat = "1054232996"
         self.node = "Nat-Fin-Gullo-01"
 
     def sQuery(self, values):
-        values.update({'rdtype': 'json', 'hash': self.hash, 'key': self.key})
+        values.update({'rdtype': 'json', 'hash': self.apihash, 'key': self.apikey})
         response = requests.get(
-            'https://'+self.url+':5656/api/client/command.php', params=values, timeout=10)
+            'https://'+self.apiurl+':5656/api/client/command.php', params=values, timeout=10)
         return response.text
 
     def get_status(self):
@@ -33,13 +33,13 @@ class solusVM_Client():
         return self.sQuery({'action': 'reboot'})
 
     def telegram(self, text):
-        values = {'chat_id': self.chat, 'text': '§'+self.node+'§ '+text}
+        values = {'chat_id': self.tgchat, 'text': '§'+self.node+'§ '+text}
         response = requests.get(
-            'https://api.telegram.org/bot'+self.bot+'/sendMessage?parse_mode=HTML', params=values, timeout=10)
+            'https://api.telegram.org/bot'+self.tgbot+'/sendMessage?parse_mode=HTML', params=values, timeout=10)
         return response.text
 
 
-if name == "__main__":
+if __name__ == "__main__":
     svmc = solusVM_Client()
     status = svmc.get_status()
     print(status)
